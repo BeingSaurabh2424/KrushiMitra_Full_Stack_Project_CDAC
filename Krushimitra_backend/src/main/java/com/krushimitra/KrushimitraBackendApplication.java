75defc6eab9506b189b0f01983af459999c677c7
Jayesh Chavan <chavanjay141@gmail.com>
2025-08-17 13:49:03 +0530

    KrushiMitra's Backend Template

---
+++ Krushimitra_backend/src/main/java/com/krushimitra/KrushimitraBackendApplication.java
@@ -0,0 +1,11 @@
+package com.krushimitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrushimitraBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KrushimitraBackendApplication.class, args);
+    }
+}
